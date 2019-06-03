9f066080a32a03fed2e7f051d2500e0a4c3a90e9
yusank <yusankurban@gmail.com>
2019-06-03 19:37:30 +0800

    feat: init project
    实现基本功能

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # digit-clock
 simple terminal digital clock implement by go .
+
+## screenshot
+
+![image](https://github.com/yusank/digit-clock/screenshot-digit-clock.png)
---
+++ clock.go
@@ -0,0 +1,209 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/nsf/termbox-go"
+)
+
+var (
+	globalWidth  = 16
+	globalHeight = 9
+	timeFormat   = defTimeFormat
+)
+
+const (
+	secondFormat  = "15:04:05"
+	defTimeFormat = "15:04"
+)
+
+type pos struct {
+	x, y int
+}
+
+func main() {
+	var (
+		stop      = make(chan bool, 0)
+		timeStr   = make(chan string, 0)
+		isShowSec bool
+	)
+
+	flagSet := flag.NewFlagSet(os.Args[0], flag.ContinueOnError)
+	flagSet.BoolVar(&isShowSec, "s", false, "is show second")
+	_ = flagSet.Parse(os.Args[1:])
+
+	if isShowSec {
+		timeFormat = secondFormat
+	}
+
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+
+	go func() {
+		ticker := time.NewTicker(1 * time.Second)
+		for t := range ticker.C {
+			timeStr <- t.Format(timeFormat)
+		}
+	}()
+
+	go func() {
+		for {
+			switch ev := termbox.PollEvent(); ev.Type {
+			case termbox.EventKey:
+				switch ev.Key {
+				case termbox.KeyCtrlQ, termbox.KeyCtrlC:
+					stop <- true
+					return
+				}
+			}
+		}
+	}()
+
+loop:
+	for {
+		select {
+		case str := <-timeStr:
+			draw(str)
+		case <-stop:
+			break loop
+		}
+
+	}
+}
+
+func checkPosition(s rune, p pos) bool {
+	block := 2
+	if globalWidth < 9 {
+		block = 1
+	}
+	var (
+		midX = (globalWidth - block) / 2
+		midY = globalHeight / 2
+
+		lastX   = globalWidth - block - 2
+		lastY   = globalHeight - 1
+		lineWid = block
+	)
+
+	// 留 2 列空
+	if p.x > lastX {
+		return false
+	}
+
+	switch s {
+	case '0':
+		if p.x < lineWid {
+			return true
+		}
+
+		if p.x > lastX-lineWid {
+			return true
+		}
+
+		return p.y == 0 || p.y == lastY
+	case '1':
+		return p.x > lastX-lineWid
+	case '2':
+		if p.y == 0 || p.y == midY || p.y == lastY {
+			return true
+		}
+
+		if p.y < midY {
+			return p.x > lastX-lineWid
+		}
+
+		return p.y > midY && p.x < lineWid
+	case '3':
+		if p.y == 0 || p.y == midY || p.y == lastY {
+			return true
+		}
+
+		return p.x > lastX-lineWid
+	case '4':
+		if p.y == midY {
+			return true
+		}
+
+		if p.x > lastX-lineWid {
+			return true
+		}
+
+		return p.y < midY && p.x < lineWid
+	case '5':
+		if p.y == 0 || p.y == midY || p.y == lastY {
+			return true
+		}
+
+		if p.y < midY {
+			return p.x < lineWid
+		}
+
+		return p.y > midY && p.x > lastX-lineWid
+	case '6':
+		if p.y == 0 || p.y == midY || p.y == lastY {
+			return true
+		}
+
+		if p.x < lineWid {
+			return true
+		}
+
+		return p.y > midY && p.x > lastX-lineWid
+	case '7':
+		if p.y == 0 {
+			return true
+		}
+		return p.x > lastX-lineWid
+	case '8':
+		if p.y == 0 || p.y == midY || p.y == lastY {
+			return true
+		}
+
+		return p.x < lineWid || p.x > lastX-lineWid
+	case '9':
+		if p.y == 0 || p.y == midY || p.y == lastY {
+			return true
+		}
+		if p.x > lastX-lineWid {
+			return true
+		}
+		return p.y < midY && p.x < lineWid
+	case ':':
+
+		if p.x > midX || p.x < midX-lineWid {
+			return false
+		}
+
+		return p.y == midY+1 || p.y == midY-1
+	}
+
+	return false
+}
+
+func draw(val string) {
+	w, _ := termbox.Size()
+	if w/len(timeFormat) < globalWidth {
+		globalWidth = w / len(timeFormat)
+	}
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	var width = globalWidth
+	for i, r := range val {
+		tempW := width * (i + 1)
+		startX := width * i
+		for y := 0; y < globalHeight; y++ {
+			for x := startX; x < tempW; x++ {
+				p := pos{x: x % width, y: y}
+				if checkPosition(r, p) {
+					termbox.SetCell(x, y+5, '▒', termbox.ColorDefault,
+						termbox.ColorCyan)
+				}
+			}
+		}
+	}
+	termbox.Flush()
+}
